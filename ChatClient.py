f17a5c24c55e1f30616ce7137de005a9fa5ff2a5
Deron <toddelon@yahoo.com.tw>
2018-09-05 16:20:03 +0800

    commit

--- ChatClient.py
+++ ChatClient.py
@@ -1,19 +1,30 @@
 from socket import socket,AF_INET,SOCK_STREAM
 from threading import Thread
+import sys
+from ast import literal_eval
 
 class Client:
-
-    Login=False
+    Token=""
+    login=False
     
     s=socket(AF_INET,SOCK_STREAM)
     
     def SendCmd(self):
         while True:
             cmd=input("")
-            if "register" in cmd or self.Login: 
+            if "register" in cmd or "login" in cmd : 
                 self.s.send(bytes(cmd,'utf-8'))
+            elif "exit" in cmd.lower():
+                print("Exit")
+                sys.exit()
             else:
-                print("Not log in yet")    
+                if self.login :    
+                    if "delete" in cmd :
+                        self.Token=""
+                        self.login=False
+                    
+                else :
+                    print("Not log in yet")    
     
     def __init__(self):
         #HOST = input('Enter host: ')
@@ -33,5 +44,27 @@ class Client:
             if not data:
                 break
             print(str(data,'utf-8'))
+            d=literal_eval(str(data,'utf-8'))
+            if d["status"] == 1 :
+                print (d["message"])
+            else:
+                if "token" in d:
+                    self.login=True
+                    self.token=d["token"]
+                if "invite" in d:
+                    inv=d["invite"]
+                    print(*inv , sep=",")
+                if "friend" in d:
+                    fri=d["friend"]
+                    print(*fri , sep=",")
+                if "post" in d:
+                    post=d["post"]
+                    p=""
+                    for i in post:
+                        p+=post["id"]
+                        p+=" : "
+                        p+=post["message"]
+                        p+="\n"
+                    print(i)
 
 Client=Client()
